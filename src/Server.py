75ffe3215c33ebcc45b83d4aa24e81a04a04c269
drdread <thatonejosh1996@gmail.com>
2017-02-19 15:21:40 -1000

    adding some prints

--- src/Server.py
+++ src/Server.py
@@ -4,6 +4,7 @@ import socketserver
 class MyUDPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
+        print("Listening")
         data = self.request[0].strip()
         socket = self.request[1]
         print("{} wrote:".format(self.client_address[0]))
@@ -13,6 +14,7 @@ class MyUDPHandler(socketserver.BaseRequestHandler):
 if __name__ == "__main__":
     HOST, PORT = "103.200.110.142", 9999
     server = socketserver.UDPServer((HOST, PORT), MyUDPHandler)
+    print("Started Server")
     server.serve_forever()
 
 
