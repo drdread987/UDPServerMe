01e8598b74e664240654fea943b2b032b5623d4e
drdread <thatonejosh1996@gmail.com>
2017-02-19 15:19:23 -1000

    changed some IPs

--- src/Server.py
+++ src/Server.py
@@ -11,7 +11,7 @@ class MyUDPHandler(socketserver.BaseRequestHandler):
         socket.sendto(data.upper(), self.client_address)
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
+    HOST, PORT = "103.200.110.142", 9999
     server = socketserver.UDPServer((HOST, PORT), MyUDPHandler)
     server.serve_forever()
 
