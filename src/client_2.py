41db27d76deed15e64ed1583f675fae60a942e41
drdread <thatonejosh1996@gmail.com>
2017-02-27 16:42:53 -1000

    trying client response

--- src/client_2.py
+++ src/client_2.py
@@ -5,14 +5,22 @@ UDP_IP = "103.200.110.142"
 UDP_PORT = 9999
 MESSAGE = bytes("HELLO, WORLD!", "utf-8")
 
+self_UDP_IP = "localhost"
+self_UDP_PORT = 9999
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 start_time = datetime.datetime.now()
+s.bind((self_UDP_IP, self_UDP_PORT))
 
 counter = 0
 while 1:
+    data, addr = s.recvfrom(1024)
     ping_start = datetime.datetime.now()
     s.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-    break
+    if data == "STOP":
+        break
+    else:
+        print(data)
 s.close()
 
 
--- src/server_2.py
+++ src/server_2.py
@@ -5,6 +5,9 @@ UDP_IP = "103.200.110.142"
 UDP_PORT = 9999
 BUFFER_SIZE = 1024
 
+target_ip = None
+target_port = 9999
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((UDP_IP, UDP_PORT))
 
@@ -13,6 +16,8 @@ while 1:
     data, addr = s.recvfrom(1024)
     if not data:
         break
+    else:
+        s.sendto("STOP", (addr, target_port))
     print(data)
 
 
