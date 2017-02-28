d5c6e21928efa85e37026a1ea423fcb6fe54192a
drdread <thatonejosh1996@gmail.com>
2017-02-27 16:34:49 -1000

    changed to UDP

--- src/client_2.py
+++ src/client_2.py
@@ -1,24 +1,19 @@
 import socket
 import datetime
 
-TCP_IP = "103.200.110.142"
-TCP_PORT = 9999
-BUFFER_SIZE = 1024
+UDP_IP = "103.200.110.142"
+UDP_PORT = 9999
 MESSAGE = bytes("HELLO, WORLD!", "utf-8")
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 start_time = datetime.datetime.now()
+
 counter = 0
 while 1:
-    s.send(MESSAGE)
-    data = s.recv(BUFFER_SIZE)
-    current_time = datetime.datetime.now()
-    counter += 1
-    if current_time - start_time > datetime.timedelta(0, 60):
-        break
+    ping_start = datetime.datetime.now()
+    s.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    break
 s.close()
-print(counter)
 
 
 
--- src/server_2.py
+++ src/server_2.py
@@ -1,19 +1,18 @@
 import socket
 
 
-TCP_IP = "103.200.110.142"
-TCP_PORT = 9999
+UDP_IP = "103.200.110.142"
+UDP_PORT = 9999
 BUFFER_SIZE = 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(5)
+s.bind((UDP_IP, UDP_PORT))
 
-conn, addr = s.accept()
+
 while 1:
-    data = conn.recv(BUFFER_SIZE)
+    data, addr = s.accept()
     if not data:
         break
-    conn.send(bytes("I SEE YOU ARE ALSO A BOT", "utf-8"))
-conn.close()
+    print(bytes(data, "utf-8"))
+
 
